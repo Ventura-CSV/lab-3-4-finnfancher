fba94a878db6b5a3c54e361aafb576526251ff01
Finn Fancher <fancherfinn@gmail.com>
2024-06-26 08:34:08 -0700

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def main():
     Code Your Program here
     ########################################
     """
+    if(x >= 0 and y >= 0):
+        quadrant = 1
+    elif(x>=0 and y < 0):
+        quadrant = 4
+    elif(x<0 and y>=0):
+        quadrant = 2
+    else:
+        quadrant = 3
 
     print(f'Quadrant: {quadrant}')
     ########################################
